ea784b39ae447427a242e5952745a734910fb33a
Adam Wienconek <adam.wienconek@new-gravity.com>
2022-05-19 16:14:06 +0200

    Initial commit

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "AppVersion",
+    name: "VersionKit",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AppVersion",
-            targets: ["AppVersion"]),
+            name: "VersionKit",
+            targets: ["VersionKit"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "AppVersion",
+            name: "VersionKit",
             dependencies: []),
     ]
 )
